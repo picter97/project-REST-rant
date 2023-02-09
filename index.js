00ef5a9b7f74b0cd1a4b9ea01540cc721b6941a9
picter <picter2esolv@gmail.com>
2023-02-09 13:37:43 -0500

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
-
-app.listen(3000)
+app.get('*',(req, res) => {
+  res.status(404).send('<h1>404 page</h1>')
+})
+app.listen(process.env.PORT)
