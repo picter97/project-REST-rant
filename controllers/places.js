1e225f5218dd08fd44af24906b9cd626838c370b
picter <picter2esolv@gmail.com>
2023-02-14 21:02:08 -0500

    submit new place is fixed

--- controllers/places.js
+++ controllers/places.js
@@ -2,23 +2,11 @@ const router = require ('express').Router()
 const places = require('../models/places.js')
 
 router.get('/', (req, res)=> {
-    console.log('controllers')
     //res.render('places/index')
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffe.jpg'
-  }]
+
   res.render('places/index', {places})
 })
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
