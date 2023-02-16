f9fecdbbed8a2b509c8787e847289de4c17bc308
picter <picter2esolv@gmail.com>
2023-02-15 20:55:54 -0500

    show route conected

--- controllers/places.js
+++ controllers/places.js
@@ -7,6 +7,19 @@ router.get('/', (req, res)=> {
   res.render('places/index', {places})
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]){
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id]})
+  }
+})
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
